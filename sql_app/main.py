306742d2bfb22540db4d1b31fbc9b9279475ee1f
kaya <kaya@treenod.com>
2021-04-26 00:04:28 +0900

    커밋

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,5 +1,13 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
 
 app = FastAPI()
 
@@ -26,3 +34,11 @@ async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
