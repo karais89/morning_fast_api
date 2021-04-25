9b3ae35ce2ff50db656701b54276a7d8c74e3cbc
kaya <kaya@treenod.com>
2021-04-25 23:51:33 +0900

    커밋

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,8 +1,28 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return fake_items_db[0:10]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
