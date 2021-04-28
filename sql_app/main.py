42526d778ebac7a776422a809333db64a44c6f4d
kaya <kaya@treenod.com>
2021-04-28 09:29:16 +0900

    완료

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,58 +1,22 @@
-from typing import Set
-from fastapi import FastAPI
-from enum import Enum
-# from config import dev
-# from sql_app.config import config
-# import configs.config
-# from utils import util
-from configs import config
+from functools import lru_cache
 
+from fastapi import Depends, FastAPI
+
+from configs import config
 
 app = FastAPI()
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@lru_cache()
+def get_settings():
+    return config.Settings()
 
 
 @app.get("/info")
-async def info():
+async def info(settings: config.Settings = Depends(get_settings)):
     return {
-        "app_name": config.settings.app_name,
-        # "admin_email": dev.settings.admin_email,
-        "items_per_user": config.settings.items_per_user,
+        "app_env": settings.app_env,
+        "app_name": settings.app_name,
+        "admin_email": settings.admin_email,
+        "items_per_user": settings.items_per_user,
     }
-    
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
