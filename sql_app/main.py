42806a17fabd1aedacf7a2e4eae0ac8ad846d179
kaya <kaya@treenod.com>
2021-04-30 12:42:45 +0900

    커밋

--- sql_app/main.py
+++ sql_app/main.py
@@ -7,11 +7,11 @@ from . import crud, models, schemas
 from .database import SessionLocal, engine
 
 # 매우 간단한 방법으로 데이터베이스 테이블을 만듭니다.
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World "}
