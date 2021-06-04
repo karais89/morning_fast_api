b946de8839b8fcae36d3af5274191fc4c9608ea6
kaya <kaya@treenod.com>
2021-06-04 09:08:37 +0900

    완료

---
+++ AUTH.md
@@ -0,0 +1,6 @@
+# 인증
+
+- https://fastapi.tiangolo.com/ko/tutorial/security/oauth2-jwt/
+- fastAPI를 보면 엄청나게 쉽게 기능을 제공하는 것으로 보임.
+- flast에서는 데코레이터를 사용하여 기능을 구현했는데, fastAPI는 dependecie를 입력하면 됨.
+- dependencies는 각 함수마다, 혹은 글로벌 하게, 혹은 각 파일에 속한 router 마다 걸 수 있음.
---
+++ main.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from fastapi import Depends, FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+async def common_parameters(q: Optional[str] = None, skip: int = 0, limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+
+@app.get("/items/")
+async def read_items(commons: dict = Depends(common_parameters)):
+    return commons
+
+
+@app.get("/users/")
+async def read_users(commons: dict = Depends(common_parameters)):
+    return commons
